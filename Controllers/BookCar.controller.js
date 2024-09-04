27ea8ef772f2ff2925142d45c2a479b0aa58b9cb
devJawad1 <adekanbija1@gmail.com>
2024-09-04 11:05:22 +0100

    done  book car controller and modal

--- Controllers/BookCar.controller.js
+++ Controllers/BookCar.controller.js
@@ -4,10 +4,27 @@ const saveBookCar = (req, res)=>{
     const {user, allcars} = req.body
 
 
-    allcars.map((car, i)=>{
+    const promises = allcars.map((car, i)=>{
         let carObj = {
             carId:car.carId,
-            owner:user
+            owner:user,
+            location:car.location,
+            picked:false,
+            delivered:false
         }
+
+        const savecar = new bookcarDB (carObj) 
+        return savecar.save();
     })
+
+    
+    Promise.all(promises)
+    .then((savedCars) => {
+        console.log(savedCars);
+        res.send({msg: `Your car${allcars.length > 1 ? 's' : ''} have been booked successfully`,status: true });
+    })
+    .catch((err) => {
+        console.log('Error while saving book car', err);
+        res.send({msg: "Oops, something went wrong. Try again in a few minutes.", status: false});
+    });
 }
