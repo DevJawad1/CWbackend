6047ec5391a51ad490ad181d5c8b6975f6031159
devJawad1 <adekanbija1@gmail.com>
2024-09-07 11:39:45 +0100

    adjust book car for wash

--- Controllers/BookCar.controller.js
+++ Controllers/BookCar.controller.js
@@ -1,32 +1,38 @@
 const bookcarDB = require('../Modals/bookcar.modal')
-
-const saveBookCar = (req, res)=>{
+const userDB = require('../Modals/register.modal')
+const saveBookCar = async(req, res)=>{
     const {user, allcars} = req.body
 
+    const findUser = await userDB.findOne({_id:user})
+    if(findUser.type=="none"){
+        res.send({msg:"You are not a membeship yet", status:false})
+    }else{
+        const promises = allcars.map((car, i)=>{
+            let carObj = {
+                carId:car.carId,
+                owner:user,
+                location:car.location,
+                picked:false,
+                delivered:false
+            }
+            
+    
+            const savecar = new bookcarDB (carObj) 
+            return savecar.save();
+        })
+    
+    
+        Promise.all(promises)
+        .then((savedCars) => {
+            console.log(savedCars);
+            res.send({msg: `Your car${allcars.length > 1 ? 's' : ''} have been booked successfully, we are going to pick them up`,status: true });
+        })
+        .catch((err) => {
+            console.log('Error while saving book car', err);
+            res.send({msg: "Oops, something went wrong. Try again in a few minutes.", status: false});
+        });
+    }
 
-    const promises = allcars.map((car, i)=>{
-        let carObj = {
-            carId:car.carId,
-            owner:user,
-            location:car.location,
-            picked:false,
-            delivered:false
-        }
-
-        const savecar = new bookcarDB (carObj) 
-        return savecar.save();
-    })
-
-
-    Promise.all(promises)
-    .then((savedCars) => {
-        console.log(savedCars);
-        res.send({msg: `Your car${allcars.length > 1 ? 's' : ''} have been booked successfully, we are going to pick them up`,status: true });
-    })
-    .catch((err) => {
-        console.log('Error while saving book car', err);
-        res.send({msg: "Oops, something went wrong. Try again in a few minutes.", status: false});
-    });
 }
 
 module.exports = {saveBookCar}
